ec6e642d0e1a183e62da8fbde67cfbb37ea92eb4
ndrewwm <moore.andrew09@gmail.com>
2024-08-04 12:59:52 -0600

    Split load_turso() into separate tasks

--- flows/dbt_build_and_load_turso.py
+++ flows/dbt_build_and_load_turso.py
@@ -14,26 +14,28 @@ import libsql_experimental as libsql
 @task
 def get_credentials() -> dict:
     """Pull in  credentials needed to interact with the databases."""
-    
+
     creds = {
         "motherduck": Secret.load("db-motherduck-token").get(),
         "turso": {
             "url": Secret.load("db-turso-spotify-url").get(),
             "token": Secret.load("db-turso-spotify-token").get(),
-        }
+        },
     }
     return creds
 
 
 @task
-def dbt_build(creds: dict) -> None:
+def dbt_build(token: str) -> None:
     """Build the dbt project."""
 
-    os.environ["DBT_SECRET_MOTHERDUCK_TOKEN"] = creds["motherduck"]
+    os.environ["DBT_SECRET_MOTHERDUCK_TOKEN"] = token
     res: dbtRunnerResult = dbtRunner().invoke(
         args=[
             "build",
             "--project-dir",
+            "./dbt_spotify/",
+            "--profiles-dir",
             "./dbt_spotify/",
             "--exclude",
             "config.materialized:view",
@@ -47,34 +49,45 @@ def dbt_build(creds: dict) -> None:
 
 
 @task
-def load_turso(creds: dict[str, str]) -> None:
-    """Load the turso database."""
+def pull_data(token: str) -> None:
+    """Pull the data from motherduck, storing it temporarily in a SQLite database."""
 
-    ### PULL ###
+    duck = duckdb.connect(f"md:my_db?motherduck_token={token}")
+    duck.sql("attach 'temp.db' (type sqlite);")
+    duck.sql(
+        "create table temp.dim_artist as select * from my_db.spotify.dim_artist;"
+    )
+    duck.sql(
+        "create table temp.dim_album as select * from my_db.spotify.dim_album;"
+    )
+    duck.sql(
+        "create table temp.dim_track as select * from my_db.spotify.dim_track;"
+    )
+    duck.sql(
+        "create table temp.fct_track_play as select * from my_db.spotify.fct_played_track;"
+    )
+    duck.sql(
+        "create table temp.rpt_track_counts as select * from my_db.spotify.rpt_track_counts;"
+    )
+    return
 
-    get_run_logger().info("Pulling tables from motherduck...")
-    duck = duckdb.connect(f"md:my_db?motherduck_token={creds['motherduck']}")
-    duck.sql("attach 'libsql_spotify.db' (type sqlite);")
-    duck.sql("create table libsql_spotify.dim_artist as select * from my_db.spotify.dim_artist;")
-    duck.sql("create table libsql_spotify.dim_album as select * from my_db.spotify.dim_album;")
-    duck.sql("create table libsql_spotify.dim_track as select * from my_db.spotify.dim_track;")
-    duck.sql("create table libsql_spotify.fct_track_play as select * from my_db.spotify.fct_played_track;")
-    duck.sql("create table libsql_spotify.rpt_track_counts as select * from my_db.spotify.rpt_track_counts;")
 
-
-    ### DUMP ###
+@task
+def generate_ddl() -> tuple[list[str], list[str]]:
+    """Dump the SQLite data to a .sql file, for execution in turso replica.
+    Returns the DDL for execution."""
 
     get_run_logger().info("Dumping local sqlite statements...")
     with open("./dump.sql", mode="w") as file:
         subprocess.call(
-            args=["sqlite3", "libsql_spotify.db", ".dump"],
+            args=["sqlite3", "temp.db", ".dump"],
             stdout=file,
         )
 
+    creates = []
+    inserts = []
     with open("./dump.sql", mode="r") as file:
         statements = file.readlines()
-        creates = []
-        inserts = []
         for statement in statements:
             if statement[:6] == "CREATE":
                 print(statement)
@@ -82,29 +95,33 @@ def load_turso(creds: dict[str, str]) -> None:
             if statement[:6] == "INSERT":
                 inserts.append(statement)
 
-    ### LOAD ###
+    return creates, inserts
+
+
+@task
+def load_turso(creates: list[str], inserts: list[str], creds: dict[str, str]) -> None:
+    """Load the turso database."""
 
     get_run_logger().info("Setting up turso replica...")
     turso = libsql.connect(
-        "turso.db", 
+        "turso.db",
         sync_url=creds["turso"]["url"],
         auth_token=creds["turso"]["token"],
     )
 
     tables = [
-        stmt.replace("CREATE TABLE ", "").split("(")[0]
-        for stmt in creates
+        statement.replace("CREATE TABLE ", "").split("(")[0] for statement in creates
     ]
     for table in tables:
         get_run_logger().info("Deleting %s...", table)
-        turso.execute(f'drop table if exists {table};')
+        turso.execute(f"drop table if exists {table};")
 
     for create in creates:
         get_run_logger().info("Executing %s", create)
         turso.execute(create)
 
     get_run_logger().info("Inserting data...")
-    values = ''.join(inserts)
+    values = "".join(inserts)
     turso.execute(values)
 
     turso.commit()
@@ -117,8 +134,10 @@ def dbt_build_and_load_turso() -> None:
     """Flow to automate the orchestration of the dbt project and loading of turso db."""
 
     creds = get_credentials()
-    dbt_build(creds)
-    load_turso(creds)
+    dbt_build(creds["motherduck"])
+    pull_data(creds["motherduck"])
+    creates, inserts = generate_ddl()
+    load_turso(creates, inserts, creds)
 
 
 if __name__ == "__main__":
