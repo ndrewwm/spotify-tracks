c97d65586662f1f19937939c65996d7bf2cc33d3
ndrewwm <moore.andrew09@gmail.com>
2024-11-13 21:31:07 -0700

    Adjustment to handle instances where we have an update in the source data for track durations
    - this is a bit hack-y, but we'll just take the maximum
    - future: make duration a property of fct_played_track, rather than dim_track

--- dbt_spotify/models/marts/dim_track.sql
+++ dbt_spotify/models/marts/dim_track.sql
@@ -9,6 +9,15 @@ summary as (
         min(played_at_mtn) as dttm_first_played
     from tracks
     group by track_id
+),
+
+-- In the instance that we have multiple values for duration, take the maximum
+duration as (
+    select
+        track_id,
+        max(duration_ms) as duration_ms
+    from tracks
+    group by track_id
 )
 
 select distinct
@@ -21,8 +30,10 @@ select distinct
     su.dttm_first_played,
     year(su.dttm_first_played::date) as yr_first_played,
     week(su.dttm_first_played::date) as wk_first_played,
-    tr.duration_ms
+    du.duration_ms
 from tracks tr
 left join summary su
     on tr.track_id = su.track_id
+left join duration du
+    on tr.track_id = du.track_id
 order by tr.track_id
